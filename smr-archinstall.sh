40aa9c4f0940ba29cb7f3fe3bca9f59c49beda7b
Miro Holopainen <smairo@outlook.com>
2024-07-11 20:23:05 +0300

    Update smr-archinstall.sh

--- smr-archinstall.sh
+++ smr-archinstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Loop through all the positional parameters
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -19,3 +18,24 @@ done
 echo "label: gpt" | sfdisk /dev/$drive_val --wipe always
 echo ",1G,U" | sfdisk /dev/$drive_val
 echo ",," | sfdisk /dev/$drive_val
+cryptsetup -y -v --type luks2 luksFormat /dev/$drive_valp2
+cryptsetup luksOpen /dev/$drive_valp2 cryptlvm
+
+pvcreate /dev/mapper/cryptlvm
+vgcreate vg /dev/mapper/cryptlvm
+lvcreate -n lv -L 64G lvswap
+lvcreate -n lv -l 100%FREE lvroot
+
+mkfs.xfs /dev/vg/lvroot
+
+swapon /dev/vg/lvswap
+mount /dev/vg/lvroot /mnt
+mkdir -p /mnt/boot
+mount /dev/$drive_valp1 /mnt/boot
+
+pacstrap -K /mnt \
+	base linux linux-firmware sof-firmware lvm2 dosfstools base-devel efibootmgr xfsprogs \
+	openssh git nano sudo networkmanager xdg-desktop-portal xdg-desktop-portal-gtk tpm2-tss xorg-xwayland \
+	amd-ucode intel-ucode pipewire plasma plasma-meta sddm tilix kate firefox
+	
+genfstab /mnt > /mnt/etc/fstab
